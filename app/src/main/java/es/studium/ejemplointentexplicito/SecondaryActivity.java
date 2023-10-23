4e44c4740f93a5408f4a6175846f23c9318ac4bd
DmytroRevaGrupoStudium <dmytroreva2001@gmail.com>
2023-10-23 11:04:42 +0200

    Initial commit

---
+++ app/src/main/java/es/studium/ejemplointentexplicito/SecondaryActivity.java
@@ -0,0 +1,19 @@
+package es.studium.ejemplointentexplicito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
